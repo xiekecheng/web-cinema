3623a3f589b7db40989b9f97c722f4419cc01a62
xkccoding <xkccoding@gmail.com>
2022-05-16 20:52:56 +0800

    ✨ feat(个人中心): 新增个人中心模块

---
+++ src/views/center/index.jsx
@@ -0,0 +1,15 @@
+/*
+ * @Author: github.com/xiekecheng
+ * @Date: 2022-05-15 17:26:58
+ * @LastEditTime: 2022-05-15 20:44:36
+ * @FilePath: /web-cinema/src/views/center/Center.jsx
+ * @Description: 
+ * 
+ */
+import React from 'react'
+
+export default function Center() {
+	return (
+		<div>Center</div>
+	)
+}
