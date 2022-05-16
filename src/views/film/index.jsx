2cf8a4eb6d8d8f6e162a1a4055e5ed4a188311ba
xkccoding <xkccoding@gmail.com>
2022-05-16 22:37:56 +0800

    🌈 style(电影模块): 样式更改
    更改正在热映/即将上映导航点击时的样式

--- src/views/film/index.jsx
+++ src/views/film/index.jsx
@@ -1,22 +1,46 @@
 /*
  * @Author: github.com/xiekecheng
  * @Date: 2022-05-15 17:26:43
- * @LastEditTime: 2022-05-15 21:01:38
+ * @LastEditTime: 2022-05-16 22:35:11
  * @FilePath: /web-cinema/src/views/film/index.jsx
  * @Description:
  *
  */
 import React from 'react';
-import { Outlet ,NavLink} from 'react-router-dom';
-
+import { Outlet, NavLink } from 'react-router-dom';
+import styles from './css/index.module.css';
 export default function Film() {
 	return (
 		<div>
-			<div style={{display:'flex',justifyContent:'space-around',padding:'10px'}}>
-				<div><NavLink to='nowPlaying'>正在热映</NavLink></div>
-				<div><NavLink to='comingSoon'>即将上映</NavLink></div>
+			<div
+				style={{
+					display: 'flex',
+					justifyContent: 'space-around',
+					padding: '10px',
+				}}
+			>
+				<div>
+					<NavLink
+						to='nowPlaying'
+						className={({ isActive }) => {
+							return isActive ? styles.active : '';
+						}}
+					>
+						正在热映
+					</NavLink>
+				</div>
+				<div>
+					<NavLink
+						to='comingSoon'
+						className={({ isActive }) => {
+							return isActive ? styles.active : '';
+						}}
+					>
+						即将上映
+					</NavLink>
+				</div>
 			</div>
-			<Outlet/>
+			<Outlet />
 		</div>
 	);
 }
