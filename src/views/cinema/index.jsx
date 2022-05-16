c1806c244ed6a63edc7d2058fdc51d4c2cbb39ea
xkccoding <xkccoding@gmail.com>
2022-05-16 20:52:28 +0800

    ✨ feat(影院): 新增影院模块

---
+++ src/views/cinema/index.jsx
@@ -0,0 +1,15 @@
+/*
+ * @Author: github.com/xiekecheng
+ * @Date: 2022-05-15 17:26:50
+ * @LastEditTime: 2022-05-15 17:27:16
+ * @FilePath: /web-cinema/src/views/Cinema.jsx
+ * @Description: 
+ * 
+ */
+import React from 'react'
+
+export default function Cinema() {
+	return (
+		<div>Cinema</div>
+	)
+}
