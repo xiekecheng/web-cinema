0825692fdb8c8e8c3e34760f58e0580c74f87325
xkccoding <xkccoding@gmail.com>
2022-05-29 00:06:16 +0800

    ✨ feat(store): 增加state reducer

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit'
 import counterReducer from './reducers/counterSlice'
+import stateReducer from './reducers/stateSlice'
 export default configureStore({
   reducer: {
-    counter:counterReducer
+    counter:counterReducer,
+    state:stateReducer
   }
 })
