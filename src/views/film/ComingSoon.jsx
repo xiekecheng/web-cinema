fbb68a7fc83d37c793ab6ab71b60b7dbff100264
xkccoding <xkccoding@gmail.com>
2022-05-29 20:52:11 +0800

    ✨ feat(正在热映&即将上映): 当页面在加载数据时,用骨架屏来显示

--- src/views/film/ComingSoon.jsx
+++ src/views/film/ComingSoon.jsx
@@ -9,6 +9,7 @@
 import React, { useEffect, useState } from 'react';
 import FilmBox from './components/FilmBox2';
 import { getComingSoonList } from '../../api';
+import { Skeleton } from 'antd-mobile';
 export default function ComingSoon() {
 	const filmList = useFilmList();
 	return (
@@ -16,6 +17,12 @@ export default function ComingSoon() {
 			{filmList.map((item) => (
 				<FilmBox data={item} key={item.filmId} />
 			))}
+			{!filmList.length && (
+				<div>
+					<Skeleton.Title animated />
+					<Skeleton.Paragraph lineCount={10} animated />
+				</div>
+			)}
 		</div>
 	);
 }
